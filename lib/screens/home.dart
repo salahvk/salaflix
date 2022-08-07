96a804eee45a189158789c9cb3be01177d093ce2
salah <salah@iocod.com>
2022-08-07 20:51:18 +0530

    trending image Api  integrated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:salafix/API/end_point.dart';
+import 'package:salafix/components/color_manager.dart';
+import 'package:salafix/provider/data_provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final provider = Provider.of<DataProvider>(context, listen: false);
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            children: [],
+          ),
+          Expanded(
+              child: ListView.builder(
+            itemBuilder: (ctx, index) {
+              String? images = provider.trend!.results![index].posterPath;
+              final newImages = "$posterApi$images";
+              return InkWell(
+                onTap: () {
+                  // getTrending();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 80,
+                    width: 120,
+                    decoration: BoxDecoration(
+                        color: ColorManager.grayLight,
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: ColorManager.grayLight)),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        newImages,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+            itemCount: 15,
+            scrollDirection: Axis.horizontal,
+          )),
+          Container(
+            height: size.height * 0.66,
+          )
+        ],
+      )),
+    );
   }
 }
